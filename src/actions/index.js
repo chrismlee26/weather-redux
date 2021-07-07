124f81694cd6ae974fc3655fcd2b0cb49540630a
CHRIS_LEE <chrismlee87@gmail.com>
2021-07-06 18:00:47 -0700

    actions, reducers and fetch() complete

---
+++ src/actions/index.js
@@ -0,0 +1,21 @@
+export const LOAD_WEATHER = 'LOAD_WEATHER'
+
+// Async w thunk!
+export const loadWeather = (zip) => {
+  return async (dispatch) => { // <-- Thunk
+      const apiKey = process.env.REACT_APP_API_KEY;
+      const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=imperial`;
+      const res = await fetch(path)
+      const json = await res.json()
+
+      const { cod, message } = json
+
+      if (cod !== 200) { // on res error
+        dispatch({ type: LOAD_WEATHER, payload: { cod, message } })
+      }
+
+      const { temp, feels_like } = json.main
+
+    dispatch({ type: LOAD_WEATHER, payload: { temp, feels_like, cod } })
+  } 
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux'
+import weatherReducer from './weatherReducer'
 
 const rootReducer = combineReducers({
-
+  weather: weatherReducer
 })
 
 export default rootReducer
+
+
+// { weather: null } <-- shape of state
